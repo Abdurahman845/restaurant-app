43512698a57d6939976c6d6b32436df35ce4efad
Abdurahman Hassan <omerabdurahman4@gmail.com>
2024-05-26 19:11:05 +0100

    completed backend

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,21 +1,41 @@
-// import { MongoClient } from 'mongodb';
+
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb+srv://omerabdurahman4:dpbokkTOVmrksvsS@cluster0.okbzf84.mongodb.net/";
 const client = new MongoClient(url, {})
 
-console.log("code run")
+const express = require('express');
+const app = express();
+console.log("App listen at port 5000");
+app.get("/", (req, resp) => {
 
-let orders = client.db("restaurant").collection("orders").insertOne({ name: "burger", price: "£15" })
-console.log("code complete")
+    resp.send("App is Working");
+    // You can check backend is working or not by 
+    // entering http://localhost:5000
 
-// MongoClient.connect(url, function(err, db) {
-//     console.log("mongo client connected")
-//     if (err) throw err;
-//   var dbo = db.db("restaurant");
-//   var myobj = { name: "burger", price: "£15" };
-//   dbo.collection("orders").insertOne(myobj, function(err, res) {
-//     if (err) throw err;
-//     console.log("1 document inserted");
-//     db.close();
-//   });
-// });
+    // If you see App is working means
+    // backend working properly
+});
+
+app.get("/orders/burgers", (req, resp) => {
+    let orders = client.db("restaurant").collection("orders").insertOne({ name: "burger", price: "£15" })
+    resp.send("Burger has been ordered");
+
+});
+
+app.get("/orders/pizzas", (req, resp) => {
+    let orders = client.db("restaurant").collection("orders").insertOne({ name: "pizzas", price: "£25" })
+    resp.send("pizza has now been ordered");
+
+});
+app.get("/orders/shawarma", (req, resp) => {
+    let orders = client.db("restaurant").collection("orders").insertOne({ name: "shawarma", price: "£35" })
+    resp.send("shawarma has now been ordered");
+});
+
+app.get("/orders", async (req, resp) => {
+    let orders = await client.db("restaurant").collection("orders").find().toArray()
+    resp.send(orders);
+});
+
+
+app.listen(5000);
